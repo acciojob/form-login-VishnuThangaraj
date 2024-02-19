6ce14d4f2eab436f999b1a5f4bb44a092ea4e884
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-19 21:56:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const actionBtn = document.querySelector("#submitBtn");
 
-actionBtn.addEventListener("click", getFormvalue;
+actionBtn.addEventListener("click", getFormvalue);
 
 function getFormvalue() {
     //Write your code here	
